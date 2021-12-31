abed717c96d94d9b8a0684b016fe34f7195b476e
esmaeil1377 <pahang1378@gmail.com>
2021-12-31 16:45:16 +0330

    ControlStatementBraces refactoring

---
+++ src/main/java/error/handler/ErrorHandler.java
@@ -0,0 +1,13 @@
+package main.java.error.handler;
+
+/**
+ * Created by Alireza on 6/28/2015.
+ */
+public class ErrorHandler {
+    public static boolean hasError = false;
+
+    public static void printError(String msg) {
+        hasError = true;
+        System.out.println(msg);
+    }
+}
