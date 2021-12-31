5e3c184b542c95729e54fdbda40d9e428dc6bc74
esmaeil1377 <pahang1378@gmail.com>
2021-12-31 18:56:23 +0330

    EmptyCatchBlock refactoring

--- src/main/java/code/generator/CodeGenerator.java
+++ src/main/java/code/generator/CodeGenerator.java
@@ -236,7 +236,9 @@ public class CodeGenerator {
         try {
             symbolTable.getNextParam(className, methodName);
             ErrorHandler.printError("The few argument pass for method");
-        } catch (IndexOutOfBoundsException e) {}
+        } catch (IndexOutOfBoundsException e) {
+            // do something?
+        }
             VarType t = VarType.Int;
             switch (symbolTable.getMethodReturnType(className, methodName))
             {
