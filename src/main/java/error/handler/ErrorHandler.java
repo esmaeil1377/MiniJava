8b4469c81caac51e273206fba497588ee27df8d0
esmaeil1377 <pahang1378@gmail.com>
2021-12-31 18:05:25 +0330

    UseUtilityClass refactoring

--- src/main/java/error/handler/ErrorHandler.java
+++ src/main/java/error/handler/ErrorHandler.java
@@ -4,7 +4,15 @@ package main.java.error.handler;
  * Created by Alireza on 6/28/2015.
  */
 public class ErrorHandler {
-    public static boolean hasError = false;
+    public static boolean isHasError() {
+        return hasError;
+    }
+
+    public static void setHasError(boolean hasError) {
+        ErrorHandler.hasError = hasError;
+    }
+
+    private static boolean hasError = false;
 
     public static void printError(String msg) {
         hasError = true;
